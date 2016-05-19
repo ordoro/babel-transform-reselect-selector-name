999086c6f938714f68db42b1515046a31e37a376
Luqmaan <ldawoodjee@gmail.com>
2016-05-18 20:57:22 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# babel-transform-reselect-selector-name
+
+[experiment]
+
+Transform calls to `reselect` `createSelector` to call with `name` as the first argument.
+
+Combine with a modified `reselect` library to debug `reselect` performance.
+
+http://astexplorer.net/#/10VeNUYnRw/7
---
+++ index.js
@@ -0,0 +1,27 @@
+export default function ({Plugin, types: t}) {
+  return {
+    visitor: {
+      VariableDeclarator(path, state) {
+        if (path.node.init.callee.name === 'createSelector') {
+          const selectorName = path.node.id.name;
+          path.node.init.arguments = [
+            t.stringLiteral(`${state.file.opts.filename}:${selectorName}`),
+            ...path.node.init.arguments
+          ];
+        }
+      },
+
+      ObjectProperty(path, state) {
+        if (!t.isCallExpression(path.node.value)) {
+          return;
+        }
+        if (path.node.value.callee.name === 'createSelector') {
+          path.node.value.arguments = [
+            t.stringLiteral(`${state.file.opts.filename}:${path.node.key.name}`),
+            ...path.node.value.arguments
+          ];
+        }
+      }
+    }
+  };
+}
